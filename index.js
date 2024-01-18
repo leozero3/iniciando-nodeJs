78788cfa1fd7cb8a8df0ac49ce556bf11a55d5e5
Leonardo Gonzalez dos Santos <59115521+leozero3@users.noreply.github.com>
2024-01-18 11:53:07 -0300

    #Configurando Nodemon e criando nossa primeira rota com query params

--- index.js
+++ index.js
@@ -8,4 +8,17 @@ server.listen(3000);
 
 server.get("/first", (req, res) => {
   return res.send({ first: "hello" });
+  // http://localhost:3000/first
+});
+
+server.get("/query-params", (req, res) => {
+  const name = req.query.name;
+  return res.json({ result: `seja bem vindo ${name}` });
+  // http://localhost:3000/query-params?name=leo
+});
+
+server.get("/query-params1", (req, res) => {
+  const { name, age } = req.query;
+  return res.json({ result: `seja bem vindo ${name} sua idade é ${age}` });
+  // http://localhost:3000/query-params1?name=leo&age=28
 });
